5c89078a894616d754c6230fa7f7e736fc800303
Hannah Carino <hcarino.official@gmail.com>
2022-12-17 11:51:07 +1300

    Added item list component with basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // Properties
   title = 'todo';
+  filter: 'all' | 'active' | 'done' = 'all';
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'practice angular', done: false },
+    { description: 'play with regan', done: false }
+  ];
+
+  // Methods
+  get items() {
+    if (this.filter === 'all'){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    })
+  }
+
 }
